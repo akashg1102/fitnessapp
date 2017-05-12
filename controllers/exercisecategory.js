f63396fdf50765f3ba81c4e2c66d818265778426
brajeshsipl <brajesh.kanungo@systematixindia.com>
2017-05-12 17:14:58 +0530

    update  exercisecategory controller.

--- controllers/exercisecategory.js
+++ controllers/exercisecategory.js
@@ -1,26 +1,42 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
- 
-// Thanks to http://blog.matoski.com/articles/jwt-express-node-mongoose/
- 
-// set up a mongoose model
-var ExerciseCategorySchema = new Schema({
-  exercisecategoryname: {
-        type: String,
-        required: true
-    },
-    exercisecategoryid: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    timestamp:{ 
-		type :Date ,
-		'default': Date.now
-		}
-  
-});
- 
- 
-module.exports = mongoose.model('ExerciseCategory', ExerciseCategorySchema);
+
+var path 			= require('path');
+var ExerciseCategory        = require('../app/models/exercisecategory');
+
+module.exports = {
+	addexercisecategory : function(req, res ,next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		
+		if (!req.body.exercisecategoryname || !req.body.exercisecategoryid) {
+				res.json({success: false, msg: 'Please  exercisecategoryname &  exercisecategoryid.'});
+			}else {
+				var addExerciseCategoryData = new ExerciseCategory({
+					exercisecategoryname: req.body.exercisecategoryname,
+					exercisecategoryid: req.body.exercisecategoryid
+					});
+					// save the exercise
+					addExerciseCategoryData.save(function(err) {
+					if (err) {
+						return res.json({success: false, msg: 'Error in add exercise category.'});
+					}
+						res.json({success: true, msg: 'Successful added new  exercise category.'});
+					}); 
+			}
+		 
+	},
+	listexercisecategory:function(req, res) {
+		ExerciseCategory.find({}, function (err, ercisecategory) {
+
+		if(!ercisecategory) {return res.json({success: false, msg: 'no ercisecategory exists.'}); }
+		res.json({success: true, msg: 'Successful found all exercisecategory.' ,data:ercisecategory});
+		}).select('exercisecategoryname exercisecategoryid');
+	},
+
+
+};
+
+
+
+
+
+
