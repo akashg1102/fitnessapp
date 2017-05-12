9045abdd26bc68d515d2ff142c3383c8e054befb
brajeshsipl <brajesh.kanungo@systematixindia.com>
2017-05-12 16:49:51 +0530

    added exercise controller to routes.js

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ var express = require('express'),
 router = express.Router(),
 path = require('path'),
 user = require('./controllers/user');
+exercise = require('./controllers/exercise');
 module.exports = function(app) {
 	router.post('/signup', user.signup);
 	router.post('/login', user.login);
